c4f89a2c854029845a77ff33c59f08b08e342942
aryansingh186 <aryansingh700108@gmail.com>
2025-11-23 20:24:59 +0530

    server js

--- Backend/Server.js
+++ Backend/Server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://rabbit-shop-zeta.vercel.app",  
+      "https://rabbit-shop-h6jd.vercel.app",  
       "http://localhost:5173"                 
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
